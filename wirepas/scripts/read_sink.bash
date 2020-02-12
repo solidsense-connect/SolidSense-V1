15ca631da842de5921e1fc740870c7cba4497c79
Eric Bree <eric.bree@solid-run.com>
2020-02-12 12:18:36 -0500

    ensure read_sink is run as user solidsense

--- wirepas/scripts/read_sink.bash
+++ wirepas/scripts/read_sink.bash
@@ -2,4 +2,4 @@
 #
 #  Print the sink configuration on the std output
 #
-python3 /opt/SolidSense/wirepas/dbus_print_sink.py
+sudo --user=solidsense python3 /opt/SolidSense/wirepas/dbus_print_sink.py
