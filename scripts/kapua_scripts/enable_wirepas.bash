525b467c9b115cc7c68f73de786e22cad62805bd
Laurent Carre <laurent.carre@sterwen-technology.eu>
2020-03-03 12:29:22 +0200

    adding MQTT client for system tests

--- scripts/kapua_scripts/enable_wirepas.bash
+++ scripts/kapua_scripts/enable_wirepas.bash
@@ -3,8 +3,6 @@
 # assume to be executed from Kpaua in /tmp
 # the SolidSense-conf-custom.yml file with Wirepas is assumed to be part of the zip file
 #
-# get the file from the server
-curl -o SolidSense-conf-custom.yml https://images.solidsense.io/SolidSense/config/SolidSense-conf-wirepas.yml
 cp -v SolidSense-conf-custom.yml /data/solidsense/config
 ls -l /data/solidsense/config
 # soft reconfig
